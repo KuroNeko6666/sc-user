72d6d231a49d460d30d2ca2ff578ab03698370e0
Agung Priyatno <102217827+KuroNeko6666@users.noreply.github.com>
2023-06-26 11:46:55 +0700

    u

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api: {
-    host: "http://localhost:8000/api/user",
+    host: "http://188.166.245.130:8000/api/user",
     path: {
       auth: "/auth",
       device: "/device",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+  api: {
+    host: "http://188.166.245.130:8000/api/user",
+    path: {
+      auth: "/auth",
+      device: "/device",
+      deviceData: "/device/data",
+      dashboard: "/dashboard/",
+      order: "/order",
+      cart: "/cart",
+    }
+  }
+};
