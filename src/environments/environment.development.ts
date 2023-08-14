4cd76a97bc8f362354e6dedae148e03d9f661493
Agung Priyatno <agungpriyatno303@gmail.com>
2023-08-14 17:49:43 +0700

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api: {
-    host: "http://188.166.245.130:8000/api/user",
+    host: "https://be-sc.mareca.vc/api/admin",
     path: {
       auth: "/auth",
       device: "/device",
